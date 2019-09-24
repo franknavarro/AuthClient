f7f632b12c57fdf3ccb11404255b12d0e5b466c7
Frank Navarro <fnavarro7777@gmail.com>
2019-09-24 15:38:49 -0700

    Added styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from './Header';
+import { Route } from 'react-router-dom';
 
 const App = ({ children }) => {
   return (
-    <div>
-      <Header />
+    <div className="ui container">
+      <Route path="/" component={Header} />
       {children}
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,29 +3,59 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 class Header extends Component {
-  renderLinks() {
+  renderLoginButtons() {
     if (this.props.authenticated) {
       return (
-        <div>
-          <Link to="/signout">Sign Out</Link>
-          <Link to="/feature">Feature</Link>
+        <div className="item">
+          <Link to="/signout" className="ui button red">
+            Sign Out
+          </Link>
         </div>
       );
     } else {
       return (
-        <div>
-          <Link to="/signup">Sign Up</Link>
-          <Link to="/signin">Sign In</Link>
-        </div>
+        <>
+          <div className="item">
+            <Link to="/signup" className="ui primary button">
+              Sign Up
+            </Link>
+          </div>
+          <div className="item">
+            <Link to="/signin" className="ui button">
+              Sign In
+            </Link>
+          </div>
+        </>
       );
+    }
+  }
+
+  activePath(isAtPath) {
+    return this.props.location.pathname === isAtPath ? 'active' : '';
+  }
+
+  renderLinks() {
+    if (this.props.authenticated) {
+      return (
+        <Link to="/feature" className={`item ${this.activePath('/feature')}`}>
+          Feature
+        </Link>
+      );
+    } else {
+      return null;
     }
   }
 
   render() {
     return (
-      <div>
-        <Link to="/">Redux Auth</Link>
-        {this.renderLinks()}
+      <div className="ui secondary menu">
+        <div className="ui container">
+          <Link to="/" className={`item ${this.activePath('/')}`}>
+            Redux Auth
+          </Link>
+          {this.renderLinks()}
+          <div className="right menu">{this.renderLoginButtons()}</div>
+        </div>
       </div>
     );
   }
