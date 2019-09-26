c574d55caf730142a1240219b76e34cbb993495c
Frank Navarro <fnavarro7777@gmail.com>
2019-09-26 12:54:46 -0700

    Changed class to className

--- src/components/auth/SignField.js
+++ src/components/auth/SignField.js
@@ -4,7 +4,7 @@ import { Field } from 'redux-form';
 export default props => {
   return (
     <div className="field">
-      <div class="ui left icon input">
+      <div className="ui left icon input">
         <i className={`${props.icon} icon`} />
         <Field {...props} />
       </div>
